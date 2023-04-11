d43c75954d70294bd30f847995c9f2d642776a24
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2023-04-10 22:39:18 -0400

    Update and rename main-activity to emailAppMain.java

--- emailAppMain.java
+++ emailAppMain.java
@@ -8,7 +8,6 @@ public class email_application
 	
 	{
 		email object = new email("Peal","Hasan");
-		/*
 		object.change_password("xyz");
 		object.set_alternate_email("peal.hasan225@dept.xyz.com");
 		object.set_mailbox_capacity(1000);
@@ -16,7 +15,6 @@ public class email_application
 		object.get_mailbox_capacity();
 		object.get_password();
 		System.out.println(object.display_information());
-		*/
 	}
 
 }
